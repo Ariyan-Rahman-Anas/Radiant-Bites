c00ec23e6bf937354f385aa4625ac878ba532582
Ariyan Rahman Anas <mohammadariyanrahmananas@gmail.com>
2024-06-18 00:55:48 +0600

    remove unnecessery code

--- src/Hooks/apiUtils.jsx
+++ src/Hooks/apiUtils.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000";
-// const BASE_URL = "https://radiant-bites-back-end.vercel.app";
+// const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://radiant-bites-back-end.vercel.app";
 
 // Function to fetch data
 export const getData = async (endpoint) => {
@@ -74,18 +74,6 @@ export const postDataWithFile = async (endpoint, data, hasFile = false) => {
 
 
 // Function to update data
-// export const updateData = async (endpoint, id, data) => {
-//   try {
-//     const response = await axios.put(`${BASE_URL}/${endpoint}/${id}`, data);
-//     return response.data;
-//   } catch (error) {
-//     console.error("Error updating data:", error);
-//     throw error;
-//   }
-// };
-
-
-// Function to update data
 export const updateData = async (endpoint, id, data) => {
   try {
     const response = await axios.patch(`${BASE_URL}/${endpoint}/${id}`, data);
@@ -103,47 +91,5 @@ export const deleteData = async (endpoint, id) => {
   } catch (error) {
     console.error("Error deleting data:", error);
     throw error;
-  }
-};
-
-
-
-
-const imageCache = {}; // Cache to store fetched images
-
-export const fetchRandomImageUrl = async (query = "") => {
-  if (imageCache[query]) {
-    console.log(`Serving cached image for query: ${query}`);
-    return imageCache[query];
-  }
-
-  // const client_id = "mDR5ewUO0kTaA1RNhC4k3DR4IxQohhq5BjOUDuPSqU0";
-  const client_id = "mDR5ewUO0kTaA1RNhC4k3DR4IxQohhq5BjOUDuPSqU0";
-  let url = `https://api.unsplash.com/photos/random?client_id=${client_id}`;
-
-  if (query) {
-    url += `&query=${query}`;
-  }
-
-  try {
-    const response = await fetch(url, {
-      headers: {
-        "Accept-Version": "v1",
-      },
-    });
-
-    if (!response.ok) {
-      console.error(`Error fetching image: ${response.statusText}`);
-      return `https://source.unsplash.com/1200x540/?${query}`; // Use source.unsplash.com as fallback
-    }
-
-    const data = await response.json();
-    const imageUrl = data.urls.regular;
-    imageCache[query] = imageUrl; // Cache the image URL
-    console.log(`Fetched and cached image URL: ${imageUrl}`);
-    return imageUrl;
-  } catch (error) {
-    console.error("Error fetching random image:", error);
-    return `https://source.unsplash.com/1200x540/?${query}`; // Use source.unsplash.com as fallback
   }
 };
